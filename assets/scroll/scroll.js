c3f5a2878c6770cb5955f7aae2793c67f60dc4f4
Nicogomez3 <nicogmz2014@hotmail.com>
2024-09-27 21:05:25 -0300

    probando fix de scrollreveal

--- assets/scroll/scroll.js
+++ assets/scroll/scroll.js
@@ -1,32 +1,34 @@
 import ScrollReveal from 'scrollreveal';
 
 const aboutContainer = document.querySelector('.about__container')
-const productNew = document.querySelector('.products__new')
+const productNew = document.querySelector('.products__new__section')
 const categoriesContainer = document.querySelector('.categories__container')
-const productContainer = document.querySelector('.products__section')
-const benefitsContainer = document.querySelector('.benefits__cards__container')
+const productContainer = document.querySelector('.products__container')
+// const benefitsContainer = document.querySelector('.benefits__cards__container')
 const formContainer = document.querySelector('.form__container')
-const cardOne = document.getElementById('card')
-const cardTwo = document.getElementById('card2')
-const cardThree = document.getElementById('card3')
-
+// const cardOne = document.getElementById('card')
+// const cardTwo = document.getElementById('card2')
+// const cardThree = document.getElementById('card3')
+const header = document.querySelector('header')
 
 
 const revealSection = (section, delay, duration, origin, distance,) => {
-    ScrollReveal().reveal(section, { delay: delay, duration: duration, origin: origin, distance: distance, reset: true });
+    ScrollReveal().reveal(section, { delay: delay, duration: duration, origin: origin, distance: distance, reset: true, mobile: true });
 }
 
 
 
 export const scrollInit = () => {
     revealSection(aboutContainer, 300, 2000, 'left', '50px')
-    revealSection(productNew, 300, 2000, 'right', '50px')
+    revealSection(productNew, 300, 2000, 'top', '50px')
     revealSection(categoriesContainer , 300, 2000, 'left', '50px')
     revealSection(productContainer, 400, 2000, 'right', '50px')
-    revealSection(benefitsContainer , 300, 2000, 'left', '50px')
+    // revealSection(benefitsContainer , 300, 2000, 'left', '50px')
     revealSection(formContainer , 500, 2000, 'bottom', '50px')
-    revealSection(cardOne, 300, 2000, 'left', '50px')
-    revealSection(cardTwo, 400, 2500, 'bottom', '50px')
-    revealSection(cardThree, 300, 3000, 'right', '50px')
+     revealSection(cardOne, 300, 2000, 'left', '50px')
+     revealSection(cardTwo, 400, 2500, 'bottom', '50px')
+     revealSection(cardThree, 300, 3000, 'right', '50px')
+     revealSection(header, 300, 1000, 'top', '50px')
     console.log(revealSection)
+ 
 }
